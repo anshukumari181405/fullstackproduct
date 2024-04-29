3b95e7c2bc3fb779c3c52a9f0b3979763f61f224
anshukumari181405 <146926520+anshukumari181405@users.noreply.github.com>
2024-04-29 23:25:41 +0530

    fullstack

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://anshukumari181405:Akash%40123@cluster0.fbsukfp.mongodb.net/fullstackproduct?retryWrites=true&w=majority&appName=Cluster0',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb+srv://anshukumari181405:Akash%40123@cluster0.fbsukfp.mongodb.net/fullstackproduct?retryWrites=true&w=majority&appName=Cluster0')
 const db = mongoose.connection;
 db.on('error', console.error.bind(console,'MongoDB connection error:'));
 db.once('open',() => {
     console.log('Connected to Mongodb');
 });
-
 //Routes
-const productsRouter = require('./routes/products');
-app.use('api/products', productsRouter);
+
+app.use('/api', productsRouter);
+
+// app.use('/',(req, res) => {
+//     res.json(({message:'homepage'}));
+// })
 
 const PORT = 8080;
 app.listen(PORT,() => {
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router();
+const productsRouter = express.Router();
 const Product = require('../models/Product');
 
 //GET all Products
-router.get('/', async (req, res) => {
+
+productsRouter.get('/', async (req, res) => {
     try{
         const products = await Product.find();
         res.json(products);
@@ -12,10 +13,14 @@ router.get('/', async (req, res) => {
         res.status(500).json({message:'Server error'});
     }
 });
+// productsRouter.get('/',(req, res) => {
+//     res.json(({message:'homepage'}));
+// })
 
 //Post a new product
-router.post('/', async (req, res) => {
+productsRouter.post('/', async (req, res) => {
     const {title, description, price, oldPrice, rating, inStock, image, category} = req.body;
+    console.log(title, description, price, oldPrice, rating, inStock, image, category);
     try{
         const newProduct = new Product({
             title,
@@ -35,8 +40,9 @@ router.post('/', async (req, res) => {
         res.status(500).json({message:'Server error'});
     }
 });
+   
 // PUT update a product by ID
-router.put('/:id', async (req, res) => {
+productsRouter.put('/:id', async (req, res) => {
     try {
       const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
       if (!updatedProduct) {
@@ -50,7 +56,7 @@ router.put('/:id', async (req, res) => {
   });
   
   // DELETE a product by ID
-  router.delete('/:id', async (req, res) => {
+  productsRouter.delete('/:id', async (req, res) => {
     try {
       const deletedProduct = await Product.findByIdAndDelete(req.params.id);
       if (!deletedProduct) {
@@ -63,4 +69,4 @@ router.put('/:id', async (req, res) => {
     }
   });
   
-module.exports = router;
+module.exports = productsRouter;
